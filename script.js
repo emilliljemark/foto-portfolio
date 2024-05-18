417b0b950e0da78373236a660499bb6b308d742f
emilliljemark <emil.liljemark@elev.ntig.se>
2024-05-18 12:39:15 +0200

    Arbete

--- script.js
+++ script.js
@@ -10,3 +10,16 @@ function closeImage() {
     document.getElementById("dimmer").style.display = "none";
     document.getElementById("imageViewer").style.display = "none";
 }
+
+// Dropdown
+
+function drop() {
+    links = document.getElementById("links");
+    console.log(links.style.display)
+    if (links.style.display == "block") {
+        links.style.display = "none";
+    }
+    else {
+        links.style.display = "block";
+    }
+}
